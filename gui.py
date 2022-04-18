edbd2d14d254808dcbb27964e5f2f5fb791f27b4
Zach Rickers <mzrickers@gmail.com>
2022-04-18 07:45:45 +0200

    added addApp function

--- gui.py
+++ gui.py
@@ -4,14 +4,22 @@ import os
 
 root = tk.Tk()
 
+def addApp():
+    filename= filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+
+
+
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
 
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="gray", bg="#263D42")
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="gray", bg="#263D42", command="addApp")
 openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="gray", bg="#263D42")
+runApps.pack()
 
 root.mainloop()
 
